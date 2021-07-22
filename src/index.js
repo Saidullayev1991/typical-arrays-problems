5f80d5fb89fbce97acf36f13c42ccf957fed86f4
Saidullayev1991 <siadullayev1991@gmail.com>
2021-07-22 17:11:01 +0600

    feat:<add function>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+const { sort } = require("semver");
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0){return 0;} 
+  else {
+  array.sort(function(a, b) {
+    return a - b;
+  }); return array[0];}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0){return 0;} 
+  else {
+    array.sort(function(a, b) {
+      return a - b;
+    }); return (array[array.length-1]);
+  }
+ 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0){return 0;} 
+  else 
+  {
+    array.map(i=>x+=i, x=0).reverse()[0]
+    return (x/array.length);
+  }
 }
